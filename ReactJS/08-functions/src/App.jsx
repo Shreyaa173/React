4930495b4d9dc3b81a0e202c5b566bff6b0c981a
Shreya Gupta <shreyaa1735@gmail.com>
2026-06-06 12:14:49 +0530

    folder-08 done

--- ReactJS/08-functions/src/App.jsx
+++ ReactJS/08-functions/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 const App = () => {
-  function btnClicked(){
+  function btnClicked() {
     console.log('hello');
   }
+
+  function inputTyping(elem) {
+    console.log(elem);
+  }
+
+  function userScroll(){
+    console.log("user is scrolling")
+  }
+
   return (
-    <div>
-      <button onClick={btnClicked}>Click</button>
+    // <div className = 'input-text'>
+    //   <input onChange = {(elem) => {
+    //       inputTyping(elem.target.value)
+    //   }} type='text'/>
+    <div onWheelCapture={userScroll}>
+      <div className="parent">
+        <div className="page-1"></div>
+        <div className="page-2"></div>
+        <div className="page-3"></div>
+      </div>
     </div>
   )
 }
